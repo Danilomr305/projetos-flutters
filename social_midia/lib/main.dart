a8100ab4c619b9c2f8e91003651fbef4f06e3a9b
Danilomr305 <danilomr305@gmail.com>
2023-07-25 14:17:28 -0300

    o

--- social_midia/lib/main.dart
+++ social_midia/lib/main.dart
@@ -74,7 +74,8 @@ class _SocialState extends State<Social> {
                       size: 25.0,
                       color: Colors.blue,
                       ),
-                    title:    Text('+55 1234-2345',
+                    title:    
+                    Text('+55 1234-2345',
                         style: TextStyle(
                           color: Colors.blue, 
                           fontFamily: 'Pacifico',
