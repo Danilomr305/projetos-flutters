ae920f05c001c77b44895624634b1e91deb3ad50
Danilomr305 <danilomr305@gmail.com>
2023-07-27 13:15:26 -0300

    u

--- social_midia/lib/main.dart
+++ social_midia/lib/main.dart
@@ -40,108 +40,110 @@ class _SocialState extends State<Social> {
     return Center(
       child: Scaffold( 
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.all(10.0), 
-              ),
-              const CircleAvatar(
-                backgroundImage: AssetImage('imagen/perfil.jpg'),
-                radius: 50.0,  
-              ),
-              const Text(
-                'Danilo Martins',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.blue,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w900,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.all(15.0), 
                 ),
-              ),
-              const SizedBox(
-                height: 5.0,
-              ),
-              const Text(
-                'DESENVOLVEDOR FLUTTER',
-                style: TextStyle(
+                const CircleAvatar(
+                  backgroundImage: AssetImage('imagen/perfil.jpg'),
+                  radius: 50.0,  
+                ),
+                const Text(
+                  'Danilo Martins',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.blue,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5.0,
+                ),
+                const Text(
+                  'DESENVOLVEDOR FLUTTER',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.w900, 
+                  ),
+                ),
+                   const SizedBox( 
+                  height: 15.0,
+                  width: 250.0,
+                 child: Divider(
+                  color: Colors.white,
+                 ),
+                ),
+               // ignore: avoid_unnecessary_containers
+                const Card(
                   color: Colors.black,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.w900, 
-                ),
-              ),
-                 const SizedBox( 
-                height: 15.0,
-                width: 250.0,
-               child: Divider(
-                color: Colors.white,
-               ),
-              ),
-             // ignore: avoid_unnecessary_containers
-              const Card(
-                color: Colors.black,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,   
-                  horizontal: 25.0, 
-                ),
-                  child: ListTile(
-                    leading:
-                      Icon(Icons.phone_callback,
-                      size: 25.0,
-                      color: Colors.blue,
-                      ),
-                    title:    
-                      Text('+55 1234-2345',
-                        style: TextStyle(
-                          color: Colors.blue, 
-                          fontFamily: 'Pacifico',
-                          fontSize: 20.0
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,   
+                    horizontal: 25.0, 
+                  ),
+                    child: ListTile(
+                      leading:
+                        Icon(Icons.phone_callback,
+                        size: 25.0,
+                        color: Colors.blue,
                         ),
-                      ),
-                  ),
-              ),
-              const Card(
-                color: Colors.black,
-                margin: EdgeInsets.symmetric(
-                  vertical: 10.0,
-                  horizontal: 25.0,
-                ),
-                  child: ListTile(
-                    leading:
-                      Icon(Icons.mail_lock_rounded,
-                      size: 25.0,
-                      color: Colors.blue,
-                      ),
-                    title:    
-                      Text('danilomr305@gmail.com',
-                        style: TextStyle(
-                          color: Colors.blue, 
-                          fontFamily: 'Pacifico', 
-                          fontSize: 20.0
+                      title:    
+                        Text('+55 1234-2345',
+                          style: TextStyle(
+                            color: Colors.blue, 
+                            fontFamily: 'Pacifico',
+                            fontSize: 20.0
+                          ),
                         ),
+                    ),
+                ),
+                const Card(
+                  color: Colors.black,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10.0,
+                    horizontal: 25.0,
+                  ),
+                    child: ListTile(
+                      leading:
+                        Icon(Icons.mail_lock_rounded,
+                        size: 25.0,
+                        color: Colors.blue,
+                        ),
+                      title:    
+                        Text('danilomr305@gmail.com',
+                          style: TextStyle(
+                            color: Colors.blue, 
+                            fontFamily: 'Pacifico', 
+                            fontSize: 20.0
+                          ),
+                        ),
+                    ),
+                ),
+              Center(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                      onPressed: (){
+                        alterarFotos();
+                        },
+                      child: Image.asset(
+                        'imagens/tela$fotosTela.jpg',
+                        height: 350.0,
                       ),
-                  ),
-              ),
-            Center(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                    onPressed: (){
-                      alterarFotos();
-                      },
-                    child: Image.asset(
-                      'imagens/tela$fotosTela.jpg',
-                      height: 350.0,
+                      ),
                     ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            ],
-            ),
+              ],
+              ),
+          ),
           ),
       ),
     );
