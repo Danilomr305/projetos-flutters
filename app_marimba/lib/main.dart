5179dc0a687ec79d234d379e388ce79e58170a38
Danilomr305 <danilomr305@gmail.com>
2023-07-29 16:05:09 -0300

    d

--- app_marimba/lib/main.dart
+++ app_marimba/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MarimbaApp());
 
+
+class MarimbaApp extends StatelessWidget {
+  const MarimbaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
