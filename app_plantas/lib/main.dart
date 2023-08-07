c66db6c073f586c4b371c2126b8e429644651c6a
Danilomr305 <danilomr305@gmail.com>
2023-08-06 21:18:13 -0300

    m

--- app_plantas/lib/main.dart
+++ app_plantas/lib/main.dart
@@ -1,20 +1,13 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const Plantas());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class Plantas extends StatelessWidget {
+  const Plantas({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const Placeholder();
   }
 }
