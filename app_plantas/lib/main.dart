ef126eb697d966858c521e785d1be889a9f489bb
Danilomr305 <danilomr305@gmail.com>
2023-08-06 21:55:23 -0300

    rr

--- app_plantas/lib/main.dart
+++ app_plantas/lib/main.dart
@@ -8,6 +8,32 @@ class Plantas extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const MaterialApp(
+      home: Clima(),
+    );
+  }
+}
+
+class Clima extends StatefulWidget {
+  const Clima({super.key});
+
+  @override
+  State<Clima> createState() => _ClimaState();
+}
+
+class _ClimaState extends State<Clima> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'imagem/fundo.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      );
   }
 }
