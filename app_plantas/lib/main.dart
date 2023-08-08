3ef8ab918b434bf9c4c51f2992cb1895c398c00d
Danilomr305 <danilomr305@gmail.com>
2023-08-07 21:31:26 -0300

    12

--- app_plantas/lib/main.dart
+++ app_plantas/lib/main.dart
@@ -55,7 +55,7 @@ class _ClimaState extends State<Clima> {
                       ),
                     ),
                     ),
-                )
+                ),
               ),
             ],
           )
