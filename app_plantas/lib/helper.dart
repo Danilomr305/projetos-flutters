e3c2415ebbf9a7749733cb0e8dfa28a04e809df4
Danilomr305 <danilomr305@gmail.com>
2023-08-09 12:46:03 -0300

    3

--- app_plantas/lib/helper.dart
+++ app_plantas/lib/helper.dart
@@ -62,7 +62,7 @@ class Helper{
     } else if (userFrases == 2 && _frasesNumber == 2) {
       _frasesNumber = 4;
     }
-    else if(_frasesNumber == 3 || _frasesNumber == 4 || _frasesNumber == 5 || _frasesNumber == 6){
+    else if(_frasesNumber == 3 || _frasesNumber == 4 || _frasesNumber == 5 || _frasesNumber == 6) {
       restart();
     }
 }
