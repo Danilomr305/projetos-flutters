7c7e6ec5a9519d7dff346d15393a918df0d5ade4
Danilomr305 <danilomr305@gmail.com>
2023-08-17 13:40:41 -0300

    6

--- app_plantas/lib/pages/flor_page.dart
+++ app_plantas/lib/pages/flor_page.dart
@@ -78,7 +78,7 @@ class _FlorPageState extends State<FlorPage> {
             Column(   
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding( 
+                Padding(  
                   padding: const EdgeInsets.all(24),
                   child: Image.network(widget.flor.brasao.replaceAll("40x40", "85x85 "),
                   ),
