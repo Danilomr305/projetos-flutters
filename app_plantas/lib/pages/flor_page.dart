79637d066218ecf0df11a38c811cb006061c10e9
Danilomr305 <danilomr305@gmail.com>
2023-08-16 23:45:46 -0300

    .

--- app_plantas/lib/pages/flor_page.dart
+++ app_plantas/lib/pages/flor_page.dart
@@ -50,6 +50,73 @@ class _FlorPageState extends State<FlorPage> {
   }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: widget.flor.cor,
+          actions: [IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: tituloPage,),
+          ],
+          bottom: const TabBar(tabs: [
+           Tab(
+             icon: Icon(Icons.stacked_line_chart),
+             text: 'Planta',
+           ),
+           Tab(
+             icon: Icon(Icons.stacked_line_chart),
+             text: 'Caracteristica',
+           ),
+          ],
+          indicatorColor: Colors.white,
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Image.network(widget.flor.brasao.replaceAll("40x40", "100x100"),
+                  ),
+                ),
+                Text(
+                  // ignore: unnecessary_string_interpolations
+                  '${widget.flor.nome}',
+                  style: const TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w800,
+                  ),
+                )
+              ],
+            ),
+            titulos()
+          ]),
+      ),
+    );
+  }
+  Widget titulos() {
+    final quantidade = widget.flor.titulo.length;
+
+    return quantidade == 0
+           // ignore: avoid_unnecessary_containers
+           ? Container(
+            child:  const Center(
+              child: Text('Nenhuma caracteristica ainda!'),
+              ),
+            )
+          : ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              return  ListTile(
+                leading: const Icon(Icons.emoji_events_outlined),
+                title: Text(widget.flor.titulo[index].Caracteristicas),
+                trailing: Text(widget.flor.titulo[index].Problema),
+              );
+            },
+            separatorBuilder: (_,__) => const Divider(), 
+            itemCount: quantidade
+          );
   }
 }
