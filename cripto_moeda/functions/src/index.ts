1e8a30cd5358123beb737528d5483de48a8da1b2
Danilomr305 <danilomr305@gmail.com>
2023-10-19 15:13:11 -0300

    f

---
+++ cripto_moeda/functions/src/index.ts
@@ -0,0 +1,8 @@
+import * as functions from "firebase-functions";
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+ export const helloWorld = functions.https.onRequest((request: any, response: { send: (arg0: string) => void }) => {
+   response.send("Hello from Firebase!");
+ });
